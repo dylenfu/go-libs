0e163b06e01353b64ed9cbc5e1bac92038b1913e
符坤 <fukun@fukundeMacBook-Pro.local>
2017-08-30 11:03:19 +0800

    exec.findPath get the executeable file path

--- env/path.go
+++ env/path.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"path"
 	"log"
+	"os/exec"
 )
 
 func SimplePath() {
@@ -11,15 +12,22 @@ func SimplePath() {
 	log.Println("env\t-", "current direction:", dir)
 	p := path.Dir(dir)
 	log.Println("env\t-","current direction process after path.Dir:", p)
+	log.Println("--------------------------------------------------------------\r\n")
 
 	p1 := path.Dir("$GOPATH/src/github.com/Loopring/ringminer/store")
-	log.Print("env\t-","path1", p1)
+	log.Print("env\t-","path1:", p1)
+	log.Println("--------------------------------------------------------------\r\n")
 
 	gopath := os.Getenv("GOPATH")
-	log.Println("env\t-","gopath", gopath)
+	log.Println("env\t-","gopath:", gopath)
+	log.Println("--------------------------------------------------------------\r\n")
 
 	env := os.Environ()
 	for _,v := range env {
-		log.Println("env\t-","os.Environ contains",v)
+		log.Println("env\t-","os.Environ contains:",v)
 	}
+	log.Println("--------------------------------------------------------------\r\n")
+
+	lp, _ := exec.LookPath(os.Args[0])
+	log.Println("env\t-", "executeable file in:", lp)
 }
