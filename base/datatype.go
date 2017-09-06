6f1d48f0d9a379c1418ea05d720704c197873e6d
符坤 <fukun@fukundeMacBook-Pro.local>
2017-09-06 11:23:18 +0800

    add simple string type demo & simple atomic demo

---
+++ base/datatype.go
@@ -0,0 +1,41 @@
+package base
+
+import (
+	"fmt"
+	"time"
+	"sync/atomic"
+)
+
+func SimpleStringType() {
+	bytes1 := []byte{'a', 'b', 'c', 'd', 'e'}
+	str1 := string(bytes1)
+	bytes2 := []byte(str1)
+	var bytes3 []byte = []byte{'a', 'b', 'c', 'd', 'e'}
+
+	println("str1", str1)
+	println("bytes2.size", len(bytes2))
+	for k, v := range bytes3 {
+		println(k, byte(v))
+		fmt.Println(k, string(v))
+	}
+}
+
+// 最小的原子操作，atomic.load保证加载的数据不被cpu读写
+func SimpleAtomic() {
+	countDown := int64(300)
+
+	var (
+		lastTimes int64
+		diff      int64
+		nowCount  int64
+		timer     = int64(5)
+	)
+
+	for {
+		nowCount = atomic.LoadInt64(&countDown)
+		diff = nowCount - lastTimes
+		lastTimes = nowCount
+		fmt.Println(fmt.Sprintf("%s down:%d down/s:%d", time.Now().Format("2006-01-02 15:04:05"), nowCount, diff/timer))
+		time.Sleep(time.Duration(timer) * time.Second)
+	}
+}
--- base/route.go
+++ base/route.go
@@ -4,62 +4,52 @@ func Route(sub string) {
 	switch sub {
 	case "hi":
 		println("Hello IPFS")
-		break
+
+	case "simple-string":
+		SimpleStringType()
+
+	case "simple-atomic":
+		SimpleAtomic()
 
 	case "simple-struct":
 		t := &Tee{3, "hello"}
 		println(t.GetTeeNum())
 		println(t.GetTeeName())
-		break
 
 	case "simple-channel":
 		ChannelDemo()
-		break
 
 	case "simple-channel-wait":
 		SimpleChannelDemo()
-		break
 
 	case "simple-interface":
 		InterfaceDemo()
 
 	case "simple-reflect1":
 		ReflectDemo1()
-		break
 
 	case "simple-reflect2":
 		ReflectDemo2()
-		break
 
 	case "simple-reflect3":
 		ReflectDemo3()
-		break
 
 	case "simple-reflect4":
 		ReflectDemo4()
-		break
 
 	case "simple-reflect5":
 		ReflectDemo5()
-		break
 
 	case "simple-reflect6":
 		ReflectDemo6()
-		break
 
 	case "reflect-judge":
 		JudgeType()
-		break
 
 	case "simple-path":
 		SimplePath()
-		break
 
 	case "simple-math":
 		SimpleMath()
-		break
-
-	default:
-		break
 	}
 }
