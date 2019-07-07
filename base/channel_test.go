9fcbf2abef41e56fea0c58dbcecacd0430dbafee
dylen <dylenfu@126.com>
2019-07-07 21:19:19 +0800

    modify channel test

--- base/channel_test.go
+++ base/channel_test.go
@@ -1,6 +1,7 @@
 package base
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -26,14 +27,14 @@ func (n *Node) Start() {
 }
 
 func (n *Node) Wait() {
-	<-n.stop
+	fmt.Println(<-n.stop)
 }
 
 func (n *Node) Close() {
 	close(n.stop)
 }
 
-func TestChannel2(t *testing.T) {
+func TestSimpleChannel2(t *testing.T) {
 	messages := make(chan string)
 
 	msg := "hi"
@@ -42,7 +43,80 @@ func TestChannel2(t *testing.T) {
 	}()
 
 	select {
-	case msg := <-messages:
-		t.Log("received message", msg)
+	case m := <-messages:
+		t.Log("received message", m)
+	}
+}
+
+// select用于在多个通道中进行选择，他会一直阻塞，直到通道数据准备就绪
+// 下面的两个例子:
+// DeadLock1,因为通道没有数据，执行时报错DeadLock,
+// DeadLock2,在select中添加default，那么select会执行default语句，也就不会造成死锁
+// DeadLock3,不给通道添加缓冲，流入后阻塞
+// DeadLock4,缓冲通道，流入后不阻塞
+// DeadLock5,通道流入与流出不在同一个gorutine，不阻塞，DeadLock6同理
+func TestSelectDeadLock1(t *testing.T) {
+	c := make(chan string)
+	select {
+		case m := <-c:
+			t.Log(m)
+	}
+}
+
+func TestSelectDeadLock2(t *testing.T) {
+	c := make(chan string)
+	select {
+		case m := <- c:
+			t.Log(m)
+		default:
+	}
+}
+
+func TestSelectDeadLock3(t *testing.T) {
+	c := make(chan string)
+	c <- "hi"
+	m := <- c
+	t.Log(m)
+}
+
+func TestSelectDeadLock4(t *testing.T) {
+	c := make(chan string, 1)
+	c <- "hi"
+	m:= <-c
+	t.Log(m)
+}
+
+func TestSelectDeadLock5(t *testing.T) {
+	c := make(chan string)
+	go func(){c <- "hi"}()
+	m:= <-c
+	t.Log(m)
+}
+
+func TestSelectDeadLock6(t *testing.T) {
+	c := make(chan string)
+	go func(){c <- "hi"}()
+	select {
+		case m := <- c:
+			t.Log(m)
+	}
+}
+
+// 测试select执行顺序
+// select case如果有计算语句，计算顺序为先左后右，先上后下
+// select case在执行时，如果多个case都有可能则随机执行
+func TestSelectExecutionOrder(t *testing.T) {
+	c := make(chan int, 5)
+
+	for i:=0; i < cap(c); i++ {
+		select {
+			case c <- 1:
+			case c <- 2:
+			case c <- 3:
+		}
+	}
+
+	for i:=0; i < cap(c); i++ {
+		t.Log(<-c)
 	}
 }
