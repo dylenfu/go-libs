b284d5fe065d61b02b15e2096706dda51d478e30
dylenfu <dylenfu@126.com>
2019-09-26 10:01:45 +0800

    base: add log

---
+++ base/log_test.go
@@ -0,0 +1,31 @@
+package base
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+var whitelist *Whitelist
+
+// Whitelist .
+type Whitelist struct {
+	log  *log.Logger
+	list map[int64]struct{} // whitelist for debug
+}
+
+func TestSimpleLog(t *testing.T) {
+	path := "/tmp/white_list.log"
+	list := []int64{12,345}
+
+	if f, err := os.OpenFile(path, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644); err == nil {
+		whitelist = new(Whitelist)
+		whitelist.log = log.New(f, "", log.LstdFlags)
+		whitelist.list = make(map[int64]struct{})
+		for _, mid := range list {
+			whitelist.list[mid] = struct{}{}
+		}
+	}
+
+	t.Log(1)
+}
---
+++ lintcode/exercise/algorithm/dfs/simple_dfs_test.go
@@ -0,0 +1,20 @@
+package dfs
+
+import "testing"
+
+/*
+假设现在有0~9一共10个数字，每个数字只能并且一定要出现一次，求它们的全排列。
+举个例子：数字1、2、3的全排列有：123、132、213、231、312、321 一共6种。
+*/
+
+/*
+深度优先搜索 Deep first search
+*/
+
+func dfs(n int) {
+
+}
+
+func TestSimpleDfs(t *testing.T) {
+
+}
